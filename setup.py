fc6aa825d67c3127a9f755dfc26a780cc159eceb
Atreya Goswami <atreyagoswami@gmail.com>
2020-09-03 17:13:49 +0530

    Minor change in environment configuration

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ import platform
 
 if platform.system() == 'Windows':
   os.system('python -m pip install virtualenv')
-  os.system('python -m venv opencv1')
-  os.system('.\opencv1\Scripts\\activate & pip install -r requirements.txt')
+  os.system('python -m venv .env')
+  os.system('.\.env\Scripts\\activate & pip install -r requirements.txt')
   
 
 if platform.system() == 'Linux':
   os.system('python -m pip install --user virtualenv')
-  os.system('python -m venv opencv2')
-  os.system('. opencv2/bin/activate; pip install --upgrade pip setuptools wheel')
-  os.system('. opencv2/bin/activate; pip install -r requirements.txt')
+  os.system('python -m venv .env')
+  os.system('. .env/bin/activate; pip install --upgrade pip setuptools wheel')
+  os.system('. .env/bin/activate; pip install -r requirements.txt')
   
 
