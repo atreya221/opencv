69d6fac90e14cc88c4c0918d3f96d8f84485df99
Atreya Goswami <64495311+atreya221@users.noreply.github.com>
2020-09-03 12:56:24 +0530

    Update script.py

--- Cats_vs_Dogs/script.py
+++ Cats_vs_Dogs/script.py
@@ -1,7 +1,8 @@
 import os
+import platform
 
 if platform.system() == 'Windows':
   os.system('.\opencv1\Scripts\\activate & python create_data.py')
 
 if platform.system() == 'Linux':
-  os.system('source opencv2/bin/activate; python create_data.py')
+  os.system('. ../opencv2/bin/activate; python create_data.py')
