1f8c8d7c2330c824d1889c2370f385c9c4290905
Atreya Goswami <atreyagoswami@gmail.com>
2020-09-03 16:52:06 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@ import platform
 
 if platform.system() == 'Windows':
   os.system('python -m pip install virtualenv')
+  os.system('python -m venv opencv1')
   os.system('.\opencv1\Scripts\\activate & pip install -r requirements.txt')
   
 
 if platform.system() == 'Linux':
   os.system('python -m pip install --user virtualenv')
+  os.system('python -m venv opencv2')
   os.system('. opencv2/bin/activate; pip install --upgrade pip setuptools wheel')
   os.system('. opencv2/bin/activate; pip install -r requirements.txt')
   
