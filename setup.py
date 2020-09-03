064f3586bd2a61bf5c228010fa17ee86afa1ab86
Atreya Goswami <atreyagoswami@gmail.com>
2020-09-03 17:53:05 +0530

    Minor change in setup tools

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ import os
 import platform
 
 if platform.system() == 'Windows':
-  os.system('python -m pip install virtualenv')
+  os.system('python -m pip install --user virtualenv')
   os.system('python -m venv .env')
-  os.system('.\.env\Scripts\\activate & pip install --upgrade pip setuptools wheel')
+  os.system('.\.env\Scripts\\activate & pip install setuptools wheel')
   os.system('.\.env\Scripts\\activate & pip install -r requirements.txt')
   
 
 if platform.system() == 'Linux':
   os.system('python -m pip install --user virtualenv')
   os.system('python -m venv .env')
-  os.system('. .env/bin/activate; pip install --upgrade pip setuptools wheel')
+  os.system('. .env/bin/activate; pip install setuptools wheel')
   os.system('. .env/bin/activate; pip install -r requirements.txt')
   
 
