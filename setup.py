cf92fc152641ed27cefae3f8d2e8d2b1cd6aed86
Atreya Goswami <atreyagoswami@gmail.com>
2020-09-03 18:23:20 +0530

    Updated setup.py for linux

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if platform.system() == 'Windows':
 if platform.system() == 'Linux':
   os.system('python -m pip install --user virtualenv')
   os.system('python -m venv .env')
-  os.system('. .env/bin/activate; pip install setuptools wheel')
+  os.system('. .env/bin/activate; pip install --upgrade pip setuptools wheel')
   os.system('. .env/bin/activate; pip install -r requirements.txt')
   
 
