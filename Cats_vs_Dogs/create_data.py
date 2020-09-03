06da3344f56304b03197caeec58f4eb203f3f3a4
Atreya Goswami <atreyagoswami@gmail.com>
2020-09-03 16:32:17 +0530

    Fixed bug in setting up virtualenv

--- Cats_vs_Dogs/create_data.py
+++ Cats_vs_Dogs/create_data.py
@@ -4,6 +4,8 @@ import zipfile
 import shutil
 import pyfastcopy
 
+# print(os.system('where python'))
+
 classes = []
 count = 0
  
--- Cats_vs_Dogs/script.py
+++ Cats_vs_Dogs/script.py
@@ -2,7 +2,7 @@ import os
 import platform
 
 if platform.system() == 'Windows':
-  os.system('.\opencv1\Scripts\\activate & python create_data.py')
+  os.system('..\opencv1\Scripts\\activate & python create_data.py')
 
 if platform.system() == 'Linux':
   os.system('. ../opencv2/bin/activate; python create_data.py')
