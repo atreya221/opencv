47114d5ce470f3fe9318e8dfa0bf855d67526332
Atreya Goswami <atreyagoswami@gmail.com>
2020-09-03 17:16:51 +0530

    Included wheel in installable packages

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import platform
 if platform.system() == 'Windows':
   os.system('python -m pip install virtualenv')
   os.system('python -m venv .env')
+  os.system('.\.env\Scripts\\activate & pip install --upgrade pip setuptools wheel')
   os.system('.\.env\Scripts\\activate & pip install -r requirements.txt')
   
 
